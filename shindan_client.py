ca1074cddae5615d2d86a7cc749253309e5ae991
Shimizu Hironobu <shimizu@siainc.jp>
2020-10-23 16:05:20 +0900

    診断メーカーのIDで行わせたい診断を指定できるようにしましたの！ 診断対象にしたい名前も指定できるように変更しましたの！！！

--- shindan_client.py
+++ shindan_client.py
@@ -3,14 +3,15 @@
 import urllib
 from pyquery import PyQuery as pq
 
-def request(url):
+def request(id, name='おじょうさま'):
     """
 
     診断メーカーの結果を取得する
 
     Parameters
     ----------
-    url : string 診断メーカーURL
+    id : string 診断メーカーのid
+    name : string 診断したい名前
     ----------
 
     Parameters
@@ -21,8 +22,8 @@ def request(url):
     """
 
     #診断結果のページを取得
-    data = urllib.parse.urlencode({'u': 'おじょうさま'}).encode('utf-8')
-    request = urllib.request.Request(url, data)
+    data = urllib.parse.urlencode({'u': name}).encode('utf-8')
+    request = urllib.request.Request('https://shindanmaker.com/' + id, data)
     response = urllib.request.urlopen(request)
     html = response.read()
     
