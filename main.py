362e787ffcfd9adf12017e0d716a73ff511d88c0
Shimizu Hironobu <shimizu@siainc.jp>
2020-10-27 16:04:14 +0900

    メッセージ送信がprintになってましたの

--- main.py
+++ main.py
@@ -36,6 +36,6 @@ async def on_message(message):
         shindan_id_list = config.get('app.shindan.id')
         random_index = random.randrange(0, len(shindan_id_list)-1, 1)
         shindan_id = shindan_id_list[random_index]
-        print(shindan_client.request(shindan_id, name=str(random.random())))
+        await message.channel.send(shindan_client.request(shindan_id, name=str(random.random())))
 
 client.run(config.get('app.discord.bot_token'))
