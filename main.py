cf7cf51ad4df314de58b37549d86245da2c85cc7
Shimizu Hironobu <shimizu@siainc.jp>
2020-10-27 15:12:03 +0900

    診断用の名前を乱数に変更

--- main.py
+++ main.py
@@ -10,23 +10,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print('message: ' + message.content)
-    print('author: ' + str(message.author))
-    print('id: ' + str(message.author.id))
-    print('----------------------------')
+    # print('message: ' + message.content)
+    # print('author: ' + str(message.author))
+    # print('id: ' + str(message.author.id))
+    # print('----------------------------')
 
     if message.content.startswith('ごきげんよう'):
         print(str(message.author.id))
         await message.channel.send('くたばりなさい ' + str(message.author))
 
-    if message.author.id == 700335402736680982:
-        await message.channel.send('....あなたのことを愛していますわ❤️ ' + str(message.author))
+    # if message.author.id == 700335402736680982:
+    #     await message.channel.send('....あなたのことを愛していますわ❤️ ' + str(message.author))
 
-    if message.content.startswith('おぱんつみせて'):
-        if message.author.id == 700335402736680982:
-            await message.channel.send('もう、恥ずかしいですわ❤️ ' + str(message.author))
-        else:
-            await message.channel.send('しね' + str(message.author))
+    # if message.content.startswith('おぱんつみせて'):
+    #     if message.author.id == 700335402736680982:
+    #         await message.channel.send('もう、恥ずかしいですわ❤️ ' + str(message.author))
+    #     else:
+    #         await message.channel.send('しね' + str(message.author))
 
     if message.content.startswith('おっぱいおおきいね'):
         await message.channel.send('Zカップですわ')
@@ -36,6 +36,6 @@ async def on_message(message):
         shindan_id_list = config.get('app.shindan.id')
         random_index = random.randrange(0, len(shindan_id_list)-1, 1)
         shindan_id = shindan_id_list[random_index]
-        await message.channel.send(shindan_client.request(shindan_id, name=str(message.author)))
+        print(shindan_client.request(shindan_id, name=str(random.random())))
 
 client.run(config.get('app.discord.bot_token'))
